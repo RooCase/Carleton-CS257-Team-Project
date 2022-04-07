3a2ee5843f21bec4f4fe589381416ddda16b3757
Roo Case <roo@roocase.org>
2022-04-06 19:52:20 -0500

    Uploading data again

--- getData.py
+++ getData.py
@@ -1,15 +1,27 @@
-#Brazenly copied from https://pypi.org/project/FlightRadarAPI/
+"""Data fetching script. Written by Roo Case, using documentation from AviationStack."""
+"""Fetching script taken from AviationStack's code samples for API requests"""
+import requests, json
 
-from FlightRadar24.api import FlightRadar24API
-fr_api = FlightRadar24API()
+params = {
+  'access_key': 'ab16a35958c85b847d791be015b60d84'
+}
 
-airline_icao = "DAL"
-flights = fr_api.get_flights(airline = airline_icao)
-airline_icao = "UAL"
-flights.append(fr_api.get_flights(airline = airline_icao))
-airline_icao = "AAL"
-flights = fr_api.get_flights(airline = airline_icao)
-airline_icao = "NKS"
-flights.append(fr_api.get_flights(airline = airline_icao))
 
-print(flights)
+
+api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
+
+api_response = api_result.json()
+
+
+with open('data.json', 'w') as f: #modified from this StackOverflow on properly storing JSON data: https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file
+    f.write(str(api_response))
+
+for flight in api_response['results']:
+    if (flight['live']['is_ground'] is False):
+        print(u'%s flight %s from %s (%s) to %s (%s) is in the air.' % (
+            flight['airline']['name'],
+            flight['flight']['iata'],
+            flight['departure']['airport'],
+            flight['departure']['iata'],
+            flight['arrival']['airport'],
+            flight['arrival']['iata']))
--- testOpenSky.py
+++ testOpenSky.py
@@ -1,9 +1,10 @@
 #Data from: The OpenSky Network, https://opensky-network.org
-from opensky_api import OpenSkyApi
-import json
-api = OpenSkyApi()
-s = api.get_states()
+import requests, json
 
-print(s)
-with open('data.txt', 'w', encoding='utf-8') as f: #modified from this StackOverflow on properly storing JSON data: https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file
-    f.write(str(s))
+result = requests.get('http://opensky-network.org/api/states/all')
+
+resultJSON = result.json()
+print(resultJSON)
+
+with open('data.json', 'w') as f: #modified from this StackOverflow on properly storing JSON data: https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file
+    f.write(str(resultJSON))
