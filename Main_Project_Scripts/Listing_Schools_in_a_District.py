1a06ba4a5ac8082a34bd216285b534df2d86ac3f
Rowen Hinrichs <rowenhinrichs@Rowens-MacBook-Pro.local>
2022-04-17 16:49:07 -0500

    Removing test code

--- Main_Project_Scripts/Listing_Schools_in_a_District.py
+++ Main_Project_Scripts/Listing_Schools_in_a_District.py
@@ -1,42 +1,4 @@
 #Rowen Hinrichs
-from Main_Project_Scripts.district import *
-from Main_Project_Scripts.school import *
-
-#Dummy schools
-school1 = School("sus", 'a')
-school2 = School("sus", 'b')
-school3 = School("amogus", 'c')
-school4 = School("balls", 'd')
-school5 = School("amogus", 'e')
-school6 = School("balls", 'f')
-school7 = School("balls", 'g')
-school8 = School("amogus", 'h')
-school9 = School("amogus", 'i')
-school0 = School("balls", 'j')
-
-#Dummy districts
-sus = District("sus", [])
-amogus = District("amogus", [])
-balls = District("balls", [])
-
-
-#Listing Schools in a District
-#Schools have districts, districts do not have schools
-#given a district, list all the schools
-#given a school, list all the other schools in its district
-#district objects will have an empty school list in them, that I then populate
-# this is basically the function code and a test put together. I'll make sure to seperate it later.
-
-
-schoolsList = [school1, school2, school3, school4, school5, school6, school7, school8, school9, school0]
-
-districtList = [sus, amogus, balls]
-
-#given list of districts and list of schools
-#School objects contain district
-#district object does NOT contain schools
-#output an array of schools that are in each district.
-#have district object contain list of schools
 
 #Lists schools in a district, given a list of all schools and a district
 def listSchools(schoolsList, District):
@@ -55,7 +17,4 @@ def listOtherSchools(schoolList, school):
                 listOfOtherSchools.remove(i)
     return listOfOtherSchools
 
-
-print(f"The schools in {amogus.name} are {listSchools(schoolsList, amogus)}.")
-print (listOtherSchools(schoolsList, school7))
 
--- Tests/test_school_and_district.py
+++ Tests/test_school_and_district.py
@@ -1,28 +1,33 @@
 from Main_Project_Scripts.district import District
 from Main_Project_Scripts.school import School
 from Main_Project_Scripts.Listing_Schools_in_a_District import *
+import unittest
 
-#Test schools
-school1 = School("District_A", 'a')
-school2 = School("District_A", 'b')
-school3 = School("District_B", 'c')
-school4 = School("District_C", 'd')
-school5 = School("District_B", 'e')
-school6 = School("District_C", 'f')
-school7 = School("District_C", 'g')
-school8 = School("District_B", 'h')
-school9 = School("District_B", 'i')
-school0 = School("District_C", 'j')
+class Test_Cases(unittest.TestCase):
+    #Test schools
+    school1 = School("District_A", 'a')
+    school2 = School("District_A", 'b')
+    school3 = School("District_B", 'c')
+    school4 = School("District_C", 'd')
+    school5 = School("District_B", 'e')
+    school6 = School("District_C", 'f')
+    school7 = School("District_C", 'g')
+    school8 = School("District_B", 'h')
+    school9 = School("District_A", 'i')
+    school0 = School("District_C", 'j')
 
-#Dummy districts
-District_A = District("District_A", [])
-District_B = District("District_B", [])
-District_C = District("District_C", [])
+    #Test districts
+    District_A = District("District_A", [])
+    District_B = District("District_B", [])
+    District_C = District("District_C", [])
 
-schoolsList = [school1, school2, school3, school4, school5, school6, school7, school8, school9, school0]
+    schoolsList = [school1, school2, school3, school4, school5, school6, school7, school8, school9, school0]
 
-#testing schools in district
-print(f"The schools in {District_A.name} are {listSchools(schoolsList, District_A)}.")
+    schoolsInDistrictA = [school1, school2, school9]
+    def DummyTest(self):
+        #testing schools in district
+        assertEquals(listSchools(schoolsList, District_A), schoolsInDistrictA)
+        print(f"The schools in {District_A.name} are {listSchools(schoolsList, District_A)}.")
 
-#testing other schools in a particular school's district.
-print (listOtherSchools(schoolsList, school7))
+        #testing other schools in a particular school's district.
+        print (listOtherSchools(schoolsList, school7))
