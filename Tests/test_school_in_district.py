79b9f2776d7457a283aaf38851fb2354e4d35b55
Roo Case <roo@roocase.org>
2022-04-16 22:07:49 -0500

    Updated class inheritance for tests

--- Tests/test_school_in_district.py
+++ Tests/test_school_in_district.py
@@ -1,16 +1,16 @@
 import unittest
 
 
-class TestSchoolInDistrict:
+class TestSchoolInDistrict(unittest.TestCase):
     def test_If_School_In_District(self):
         school1 = "Pacific Ridge High School" #School object, given as input
         district1 = "Carlesbad School District" #District field
-        assertEquals(school1, district1)
+        self.assertEquals(school1, district1)
 
     def test_If_District_In_School(self):
         school2 = "Pacific Ridge High School" #School field
         district2 = "Carlesbad School District" #District Object, given as input
-        assertEquals(school2, district2)
+        self.assertEquals(school2, district2)
 
 if __name__ == '__main__':
     unittest.main()
