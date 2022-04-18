93bab09e93381dd146339d5c5b89c8bd0406568b
Roo Case <roo@roocase.org>
2022-04-17 22:30:27 -0500

    added non-CLI functionality

--- Main_Project_Scripts/main.py
+++ Main_Project_Scripts/main.py
@@ -1,3 +1,10 @@
+"""
+Non-CLI functions written by Roo Case.
+
+This is the main python file, that has the highest amount of abstraction. The code for our command-line-interface is
+here, as well as the helper functions that are a part of that.
+"""
+
 import district
 from Main_Project_Scripts import csvManipulation
 from Main_Project_Scripts.csvManipulation import findIndividualGroups, findLines, createSchool, createDistrict
@@ -15,29 +22,33 @@ def importSchools():
     return listOfSchools
 
 
-def importDistricts():
+def importDistricts(schools):
     """
     Default helper script for setting up a list of districts.
+    :param: a list of all schools
     :return: a list of districts
     """
     listOfDistricts = []
     parsedCSV = findIndividualGroups(findLines("/Users/roocase/Documents/CS257/team-project-team-a/Data/Minnesota_Districts_LearningModelData_Modified.csv"))
 
     for group in parsedCSV:
-        listOfDistricts.append(createDistrict(group))
+        district = (createDistrict(group, schools))
+        if district != None:
+            listOfDistricts.append(district)
     return listOfDistricts
 
 def list_districts(districtList):
-    ans = ''
+    output = ''
     for district in districtList:
-        ans += f'{district.name}\n'
-    return ans
+        output += f'{district.name}\n'
+    return output
+
+def setup():
+    schools = importSchools()
+    districts = importDistricts(schools)
+
+    return schools, districts
 
 if __name__ == '__main__':
-    schools = importSchools()
-    districts = importDistricts()
+    schools, districts = setup()
 
-    for school in schools:
-        print(school.get_school_info())
-    for district in districts:
-        print(district.get_district_data())
