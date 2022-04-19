ddc23af3990c9a6dd3e0aab8901854a025f38b0f
Batmend Batsaikhan <67634086+batmendbar@users.noreply.github.com>
2022-04-18 20:27:31 -0500

    Documented the test cases for CLI

--- Tests/test_CLI.py
+++ Tests/test_CLI.py
@@ -16,6 +16,10 @@ dummy_district = district.District("DummyDistrict", 69, [dummy_school], dummy_gr
  
 class TestCLI(unittest.TestCase):
     def test_find_school_info_by_name(self):
+        """
+        Tests if the find_school_info_by_name is returning a correct string.
+        :return: nothing
+        """
         info = f"""
 School name: Dummy Name.
 This is a charter school with a student body of 1234 students.
@@ -24,6 +28,10 @@ These grades are available: 12
         self.assertEqual(info, main.find_school_info_by_name([dummy_school], "Dummy Name"))
         
     def test_find_district_info_by_name(self):
+        """
+        Tests if the find_district_info_by_name is returning a correct string.
+        :return: nothing
+        """
         info = f"""District name: DummyDistrict
 Enrollment for this district: 69
 List of schools:
@@ -32,12 +40,20 @@ List of schools:
         self.assertEqual(info, main.find_district_info_by_name([dummy_district], "DummyDistrict"))
 
     def test_list_objects(self):
+        """
+        Tests if the list_objects is returning a correct string.
+        :return: nothing
+        """
         info = f"""DummyDistrict
 """
         self.assertEqual(info, main.list_objects([dummy_district]))
         print(main.list_objects([dummy_district]))
     
     def test_get_weekly_data(self):
+        """
+        Tests if the get_weekly_data is returning a correct string.
+        :return: nothing
+        """
         info = f"""           Grade 12
 Week 1  AllDistance
 Week 2  AllDistance
