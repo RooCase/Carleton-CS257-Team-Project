ad582175ef5ccea38af79d457b64b9ac99e04c7e
Batmend Batsaikhan <67634086+batmendbar@users.noreply.github.com>
2022-04-18 20:29:12 -0500

    Documented tests for district class

--- Tests/test_district_class.py
+++ Tests/test_district_class.py
@@ -6,11 +6,19 @@ class TestDistrict(unittest.TestCase):
     sampleDistrict = district.District("DummyDistrict", 69, [sampleSchool])
 
     def test_get_district_info(self):
+        """
+        Tests if get_district_info in District class returns the correct string
+        :return: None
+        """
         retrievedDistrictData = self.sampleDistrict.get_district_data()
         actualDistrictData = "District name: DummyDistrict\nEnrollment for this district: 69\nList of schools:\n  DummySchool\n"
         self.assertEqual(retrievedDistrictData, actualDistrictData) 
 
     def test_weekly_mode_tracker(self):
+        """
+        Tests if weekly_mode_tracker in District class returns the correct list
+        :return: None
+        """
         self.sampleDistrict.add_week(['AllDistance', 'AllDistance', 'AllDistance', 'AllDistance', 'AllDistance', 'AllDistance', 'AllDistance', 'AllDistance', 'AllDistance', 'AllDistance10', 'AllDistance', 'AllDistance'])
         assert(self.sampleDistrict.get_weeks_for_grade(10), ['AllDistance'])
 
