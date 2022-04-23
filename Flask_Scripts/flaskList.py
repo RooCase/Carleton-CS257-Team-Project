81b61512fcd0dfcab1461a6a23a6731e41a9ab9b
Roo Case <roo@roocase.org>
2022-04-23 11:04:08 -0500

    added dlask list object functionality

---
+++ Flask_Scripts/flaskList.py
@@ -0,0 +1,22 @@
+"""
+Deals with all the Flask operations for the School object.
+Written by Roo Case
+"""
+from flask import Flask, jsonify
+from main import setup, list_objects
+
+app = Flask(__name__)
+
+
+@app.route('/list/schools')
+def listSchools():
+    schools, districts = setup()
+    return list_objects(schools)
+
+@app.route('/list/districts')
+def listDisctricts():
+    schools, districts = setup()
+    return list_objects(schools)
+
+app.run(host='0.0.0.0', port=81)
+
--- Flask_Scripts/flaskSchool.py
+++ Flask_Scripts/flaskSchool.py
@@ -12,15 +12,20 @@ app = Flask(__name__)
 """
 Function for getting general information about a specific school.
 """
+
+
 @app.route('/school/<school_name>')
 def print_school_info(school_name):
     # adding spaces before capital letters
     actual_name = re.sub(r"(\w)([A-Z])", r"\1 \2", school_name)
     return find_school_info_by_name(importSchools(), actual_name)
 
+
 """
 Function for getting school-specific detailed covid data.
 """
+
+
 @app.route('/school/<school_name>/covid')
 def print_school_covid_data(school_name):
     # adding spaces before capital letters
@@ -30,5 +35,6 @@ def print_school_covid_data(school_name):
     df_list = df.values.tolist()
     json_data = jsonify(df_list)
     return json_data
+
 
 app.run(host='0.0.0.0', port=81)
