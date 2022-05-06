7c8e08cc55372189175c710d01250e5ae18eaedd
Rowen Hinrichs <rowenhinrichs@Rowens-MacBook-Pro.local>
2022-05-06 16:30:09 -0500

    Major revisions: The tests actually pass now

--- Main_Project_Scripts/test_school_and_district.py
+++ Main_Project_Scripts/test_school_and_district.py
@@ -6,40 +6,51 @@ import unittest
 
 class Test_Cases(unittest.TestCase):
     #Test schools
-    school1 = school.School('No', "District_A", 'Dummy1', 420, [1, 2, 3, 4, 5])
-    school2 = school.School('No', "District_A", 'Dummy2', 1000, [1, 2, 3, 4, 5])
-    school3 = school.School('No', "District_B", 'Dummy3', 1000, [1, 2, 3, 4, 5])
-    school4 = school.School('No', "District_C", 'Dummy4', 1000, [1, 2, 3, 4, 5])
-    school5 = school.School('No', "District_B", 'Dummy5', 1000, [1, 2, 3, 4, 5])
-    school6 = school.School('No', "District_C", 'Dummy6', 8008, [1, 2, 3, 4, 5])
-    school7 = school.School('No', "District_C", 'Dummy7', 1000, [1, 2, 3, 4, 5])
-    school8 = school.School('No', "District_B", 'Dummy8', 69, [1, 2, 3, 4, 5])
-    school9 = school.School('No', "District_A", 'Dummy9', 1000, [1, 2, 3, 4, 5])
-    school0 = school.School('No', "District_C", 'Dummy0', 1000, [1, 2, 3, 4, 5])
-
-    #Test districts
-    District_A = district.District("District_A", 10, [], [1, 2, 3, 4, 5])
-    District_B = district.District("District_B", 20, [], [1, 2, 3, 4, 5])
-    District_C = district.District("District_C", 30, [], [1, 2, 3, 4, 5])
-
-    Test1Expected = [school1, school2, school9]
-    Test2Expected = [school3, school8]
+   
 
 
-    schoolsList = [school1, school2, school3, school4, school5, school6, school7, school8, school9, school0]
+    
 
-    #A test for the listSchools function
-    #:param: List of all test schools, test district object, expected result
-    #:return: list of schools in test district
-    def testDummyTestSchoolsInDistrict(self, schoolsList, District_A, Test1Expected):
-        self.assertEquals(Listing_Schools_in_a_District.listSchools(schoolsList, District_A), Test1Expected)
+    # A test for the listSchools function
+    # :param: List of all test schools, test district object, expected result
+    # :return: list of schools in test district
+    def setUp(self):
+
+         #Test districts
+        self.District_A = district.District("District_A", 10, [], [1, 2, 3, 4, 5])
+        self.District_B = district.District("District_B", 20, [], [1, 2, 3, 4, 5])
+        self.District_C = district.District("District_C", 30, [], [1, 2, 3, 4, 5])
+
+        self.school1 = school.School('No', self.District_A, 'Dummy1', 420, [1, 2, 3, 4, 5])
+        self.school2 = school.School('No', self.District_A, 'Dummy2', 1000, [1, 2, 3, 4, 5])
+        self.school3 = school.School('No', self.District_B, 'Dummy3', 1000, [1, 2, 3, 4, 5])
+        self.school4 = school.School('No', self.District_C, 'Dummy4', 1000, [1, 2, 3, 4, 5])
+        self.school5 = school.School('No', self.District_B, 'Dummy5', 1000, [1, 2, 3, 4, 5])
+        self.school6 = school.School('No', self.District_C, 'Dummy6', 8008, [1, 2, 3, 4, 5])
+        self.school7 = school.School('No', self.District_C, 'Dummy7', 1000, [1, 2, 3, 4, 5])
+        self.school8 = school.School('No', self.District_B, 'Dummy8', 69, [1, 2, 3, 4, 5])
+        self.school9 = school.School('No', self.District_A, 'Dummy9', 1000, [1, 2, 3, 4, 5])
+        self.school0 = school.School('No', self.District_C, 'Dummy0', 1000, [1, 2, 3, 4, 5])
+
+         
+
+        self.schoolsList = [self.school1, self.school2, self.school3, 
+        self.school4, self.school5, self.school6, self.school7, self.school8, self.school9, self.school0]
+
+       
+    def testDummyTestSchoolsInDistrict(self):
+        Test1Expected = [self.school1, self.school2, self.school9]
+        Test1 = Listing_Schools_in_a_District.listSchools(self.schoolsList, self.District_A)
+        self.assertEquals(Test1, Test1Expected)
 
 
     #A test for the listOtherSchools function
     #:param: list of all schools, school object, expected result
     #:return: a list of schools that share school5's district.
-    def testDummyTestOtherSchools(self, schoolsList, school5, Test2Expected):
-        self.assertEquals(Listing_Schools_in_a_District.listOtherSchools(schoolsList, school5), Test2Expected)
+    def testDummyTestOtherSchools(self):
+        Test2Expected = [self.school3, self.school8]
+        self.assertEquals(Listing_Schools_in_a_District.listOtherSchools(self.schoolsList, self.school5), Test2Expected)
+
 
 if __name__ == '__main__':
     unittest.main()
