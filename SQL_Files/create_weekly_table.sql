2df4cd80f33459dfe1d97539432064cd3c178966
Roo Case <roo@roocase.org>
2022-05-25 09:39:53 -0500

    renamed per advisory

--- SQL_Files/create_weekly_table.sql
+++ SQL_Files/create_weekly_table.sql
@@ -1,18 +1,18 @@
 DROP TABLE IF EXISTS weekly_modes;
 CREATE TABLE  weekly_modes (
-    object_name varchar(255),
-    time_period varchar(255),
-    k varchar(255),
-    gr1 varchar(255),
-    gr2 varchar(255),
-    gr3 varchar(255),
-    gr4 varchar(255),
-    gr5 varchar(255),
-    gr6 varchar(255),
-    gr7 varchar(255),
-    gr8 varchar(255),
-    gr9 varchar(255),
-    gr10 varchar(255),
-    gr11 varchar(255),
-    gr12 varchar(255)
+    name varchar(255),
+    weekDates varchar(255),
+    kindergarten varchar(255),
+    grade1 varchar(255),
+    grade2 varchar(255),
+    grade3 varchar(255),
+    grade4 varchar(255),
+    grade5 varchar(255),
+    grade6 varchar(255),
+    grade7 varchar(255),
+    grade8 varchar(255),
+    grade9 varchar(255),
+    grade10 varchar(255),
+    grade11 varchar(255),
+    grade12 varchar(255)
 )
