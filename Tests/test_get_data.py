66e3c4cd7df5a7e031dfc556d3505a890b6139e3
Roo Case <roo@roocase.org>
2022-05-28 09:34:47 -0500

    renamed per advisory

--- Tests/test_get_data.py
+++ Tests/test_get_data.py
@@ -1,5 +1,5 @@
 import unittest
-import example
+
 
 class MyTestCase(unittest.TestCase):
 
@@ -8,8 +8,9 @@ class MyTestCase(unittest.TestCase):
                             "Schools,518,10/3/20,10/9/20,Virtual,Virtual,Virtual,K: AllDistance; Gr1: AllDistance; " \
                             "Gr2: AllDistance; Gr3: AllDistance; Gr4: AllDistance; Gr5: AllDistance,Gr6: AllDistance; " \
                             "Gr7: AllDistance; Gr8: AllDistance,Gr9: NA; Gr10: NA; Gr11: NA; Gr12: NA,,,55 "
-        school_data_actual = example.getSchoolData("Academia Cesar Chavez Charter School")
-        self.assertEqual(school_data_actual, school_data_dummy)  # add assertion here
+        with open("example.csv", 'r') as f:
+            school_data_actual = f.read()
+            self.assertIn(school_data_dummy, school_data_actual)  # add assertion here
 
 if __name__ == '__main__':
     unittest.main()
--- Tests/test_school_in_district.py
+++ Tests/test_school_in_district.py
@@ -5,12 +5,12 @@ class TestSchoolInDistrict(unittest.TestCase):
     def test_If_School_In_District(self):
         school1 = "Pacific Ridge High School" #School object, given as input
         district1 = "Carlesbad School District" #District field
-        self.assertEquals(school1, district1)
+        self.assertEqual(school1, district1)
 
     def test_If_District_In_School(self):
         school2 = "Pacific Ridge High School" #School field
         district2 = "Carlesbad School District" #District Object, given as input
-        self.assertEquals(school2, district2)
+        self.assertEqual(school2, district2)
 
 if __name__ == '__main__':
     unittest.main()
